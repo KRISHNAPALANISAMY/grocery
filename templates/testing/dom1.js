4df7031b7fa9ac8c57cd9b16b678dd54b966f857
KRISHNAPALANISAMY <krishna29f2004.com>
2025-12-23 13:59:13 +0530

    server file

---
+++ templates/testing/dom1.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("main-title");
+console.log(title);
+const first = document.getElementsByClassName("movie-container");
+const second = document.getElementsByClassName("movie-grid");
+const third = document.getElementsByClassName("movie");
+console.log(first);
+console.log(second);
+console.log(third);
